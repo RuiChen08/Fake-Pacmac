505d81e53401768081c523e73b9c9e88df78c7f9
Rui  Chen <rui.chen@anu.edu.au>
2018-09-22 06:03:04 +1000

    Added a new class Foods as there are non-fixed number of dots

---
+++ app/src/main/java/com/example/chenrui/game1942application/Foods.java
@@ -0,0 +1,11 @@
+package com.example.chenrui.game1942application;
+
+import java.util.ArrayList;
+
+/**
+ * Author: Rui Chen
+ * Date: 2018/9/22
+ */
+
+public class Foods extends ArrayList<Food> {
+}
