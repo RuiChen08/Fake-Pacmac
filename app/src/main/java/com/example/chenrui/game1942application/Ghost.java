4ed56707f683cfc30d41cbe4b9c8bbf454feef8e
Ruiyi Sun <u6233314@anu.edu.au>
2018-10-17 18:05:05 +1100

    improve blue mode Ai

--- app/src/main/java/com/example/chenrui/game1942application/Ghost.java
+++ app/src/main/java/com/example/chenrui/game1942application/Ghost.java
@@ -17,7 +17,7 @@ abstract class Ghost {
     boolean blueMode = false;
     int time = timeInBlue;
     Pos pos;
-
+    Pos.Direction direction = Pos.Direction.Stay;
     Ghost(Pos pos){
         this.pos = pos;
     }
@@ -44,7 +44,7 @@ abstract class Ghost {
     }
 
     /*
-     * Author: Rui Chen
+     * Author: Rui Chen,Ruiyi Sun
      * Date: 15/10/2018
      *
      * Common-Used codes
@@ -52,14 +52,56 @@ abstract class Ghost {
     void getMove(Pos.Direction direction){
         Pos p = new Pos(this.pos);
 
-        if (direction == Pos.Direction.Up) pos.y -= Maze.offsetH * 2;
-        else if (direction == Pos.Direction.Down) pos.y += Maze.offsetH * 2;
-        else if (direction == Pos.Direction.Right) pos.x += Maze.offsetW * 2;
-        else if (direction == Pos.Direction.Left) pos.x -= Maze.offsetW * 2;
+        if (direction == Pos.Direction.Up) {pos.y -= Maze.offsetH * 2; this.direction = Pos.Direction.Down;}
+        else if (direction == Pos.Direction.Down) {pos.y += Maze.offsetH * 2;this.direction = Pos.Direction.Up;}
+        else if (direction == Pos.Direction.Right) {pos.x += Maze.offsetW * 2;this.direction = Pos.Direction.Left;}
+        else if (direction == Pos.Direction.Left) {pos.x -= Maze.offsetW * 2;this.direction = Pos.Direction.Right;}
 
         // backing up the original position if current position is inside the wall
         if (Maze.maze[(int) (this.pos.y / (2 * Maze.offsetH))][(int) (this.pos.x / (2*Maze.offsetW))] == 3) {
             this.pos = p;
+            changeDirection(direction);
         }
     }
+    /*
+     * Author: Ruiyi Sun
+     * Date: 17/10/2018
+     *
+     * when the ghost cannot move to the current position, it will change the position
+     */
+
+    void changeDirection(Pos.Direction PacManDirection){
+       Pos.Direction[] directions = new Pos.Direction[] {Pos.Direction.Up,Pos.Direction.Down ,Pos.Direction.Left,Pos.Direction.Right};
+        for(int i = 0;i<directions.length;i++){
+            if(PacManDirection != directions[i] && this.direction != directions[i]){
+                this.direction = directions[i] ;
+                run();
+                if(this.direction != Pos.Direction.Stay){
+                    break;
+                }
+            }
+        }
+    }
+
+    /*Author :Ruiyi Sun
+    *Date: 17/10/2018
+    *
+    * this is just copy and paste the PacMan code If anyone can help me fix this problem!!!!!!!!!!
+    *
+     */
+    void run(){
+        //Todo : simplify this code copied from PacMan!!
+        Pos p = new Pos(this.pos);
+
+        if (direction == Pos.Direction.Up) pos.y -= Maze.offsetH * 2;
+        else if (direction == Pos.Direction.Down) pos.y += Maze.offsetH * 2;
+        else if (direction == Pos.Direction.Right) pos.x += Maze.offsetW * 2;
+        else if (direction == Pos.Direction.Left) pos.x -= Maze.offsetW * 2;
+        // backing up the original position if current position is inside the wall
+        if (Maze.maze[(int) (this.pos.y / (2 * Maze.offsetH))][(int) (this.pos.x / (2*Maze.offsetW))] >= 3) {
+            this.pos = p;
+            this.direction = Pos.Direction.Stay;
+        }
+    }
+
 }
